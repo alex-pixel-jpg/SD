aa0a4432a10e71e8204388267e747330d801bd8c
alex-pixel-jpg <bealexon@gmail.com>
2026-02-08 19:14:00 +0000

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '🐌', '💗', '💓'],  // Heart emojis
+        hearts: ['❤️', '🦒', '🐌', '💗', '💐'],  // Heart emojis
         bears: ['🦔', '🌻']                       // Cute bear emojis
     },
 
